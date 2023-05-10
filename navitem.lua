917a5da6a8ea492d3d0bfa5aabd104def596fb07
Alexis Garado <alexisgarado@gmail.com>
2023-05-09 20:50:47 -0700

    [feat] background navitem

--- navitem.lua
+++ navitem.lua
@@ -36,20 +36,75 @@ setmetatable(textbox, {
 
 function textbox:select_on()
   self.selected = true
-  self.widget.markup = colorize("Selected", beautiful.fg)
+  self.widget.markup = colorize(self.widget.text, beautiful.primary[400])
   if self.custom_on then self:custom_on() end
 end
 
 function textbox:select_off()
   self.selected = false
-  self.widget.markup = colorize("Not selected", beautiful.fg)
+  self.widget.markup = colorize(self.widget.text, beautiful.fg)
   if self.custom_off then self:custom_off() end
 end
 
 function textbox:release()
+  self.widget:emit_signal("button::press")
+end
+
+-- █▄▄ ▄▀█ █▀▀ █▄▀ █▀▀ █▀█ █▀█ █░█ █▄░█ █▀▄ 
+-- █▄█ █▀█ █▄▄ █░█ █▄█ █▀▄ █▄█ █▄█ █░▀█ █▄▀ 
+
+local background = {}
+setmetatable(background, {
+  __call  = function(cls, ...)
+    return base(cls, ...)
+  end,
+})
+
+function background:select_on()
+  self.selected = true
+  self.widget.bg = beautiful.neutral[800]
+  if self.custom_on then self:custom_on() end
+end
+
+function background:select_off()
+  self.selected = false
+  self.widget.bg = beautiful.neutral[900]
+  if self.custom_off then self:custom_off() end
+end
+
+function background:release()
+end
+
+
+-- █▄▄ █░█ ▀█▀ ▀█▀ █▀█ █▄░█ 
+-- █▄█ █▄█ ░█░ ░█░ █▄█ █░▀█ 
+
+local btn = {}
+setmetatable(btn, {
+  __call  = function(cls, ...)
+    return base(cls, ...)
+  end,
+})
+
+function btn:select_on()
+  self.selected = true
+  self.widget:emit_signal("mouse::enter")
+  if self.custom_on then self:custom_on() end
+end
+
+function btn:select_off()
+  self.selected = false
+  self.widget:emit_signal("mouse::leave")
+  if self.custom_off then self:custom_off() end
+end
+
+function btn:release()
+  self.widget:emit_signal("button::press")
 end
 
 return {
   base = base,
   textbox = textbox,
+  background = background,
+  btn = btn,
 }
